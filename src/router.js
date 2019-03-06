436cd3374b3233d7c1c3b3f5ed5d5588d6081b8e
Dallas98 <990259227@qq.com>
2019-03-06 23:50:05 +0800

    首个VUE项目

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Settings from './views/Settings.vue'
+import Change from './views/Change.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Settings
+    },
+
+  ]
+})
---
+++ src/views/Settings.vue
@@ -0,0 +1,13 @@
+<script src="home.js"></script>
+<template>
+  <settings></settings>
+</template>
+<script>
+import Settings from '@/components/Settings'
+export default {
+  name: 'settings-page',
+  components: {
+    Settings
+  }
+}
+</script>
